5ff98e9f5736b4de7bcabb07ef8651478d5f3b24
Jonathan-Majors <izzymajors@gmail.com>
2021-03-15 19:02:12 -0500

    MVP

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,33 @@
 exports.checkAccountPayload = (req, res, next) => {
+  if (!req.body.name || req.body.name.trim()) {
+    res.status(40).json({
+      message: 'name and budget are required'
+    })
+  }
   // DO YOUR MAGIC
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
+  if (!req.params.id || req.body.name.trim()) {
+    res.status(400).json({
+      message: 'that name is taken'
+    })
+  }
   // DO YOUR MAGIC
 }
 
 exports.checkAccountId = async (req, res, next) => {
+  try{
+    const account = await Account.findById(req.params.id)
+    if (account) {
+      res.status(404).json({
+        message: 'account not found',
+      })
+    } else {
+      next()
+    }
+
+  }
   // DO YOUR MAGIC
 }
+
